0c3ffbb631938b71a9c0a3aab7a49c1ed7bc4b68
Michael Bauer <mihi@lo-res.org>
2014-07-09 09:54:54 +0200

    added basic templates

--- cntrct/settings.py
+++ cntrct/settings.py
@@ -12,17 +12,18 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
+SITE_ID = 0 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't%8i$2s7iwe9s2+vfp*yii59om+x)sq$8=rp3(@k@e@8i=24#('
+SECRET_KEY = os.environ.get('SECRET_KEY',
+    't%8i$2s7iwe9s2+vfp*yii59om+x)sq$8=rp3(@k@e@8i=24#(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG','true') == 'true'
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
@@ -36,6 +37,17 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'reversion',
+    'djangocms_text_ckeditor',
+    'cms',
+    'mptt',
+    'menus',
+    'sekizai',
+    'djangocms_admin_style',
+    'django.contrib.admin',
+    'django.contrib.messages',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -45,6 +57,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+    'cms.middleware.language.LanguageCookieMiddleware',  
 )
 
 ROOT_URLCONF = 'cntrct.urls'
@@ -55,17 +71,15 @@ WSGI_APPLICATION = 'cntrct.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config();
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
@@ -75,8 +89,46 @@ USE_L10N = True
 
 USE_TZ = True
 
+LANGUAGES = [
+    ('en', 'english'),
+    ]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS= (
+    os.path.join(BASE_DIR, 'cntrct', 'templates'),
+    )
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'cms.context_processors.cms_settings',
+    'sekizai.context_processors.sekizai',
+)
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'cntrct', 'static'),
+    )
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+    )
+
+CMS_TEMPLATES = (
+    ('template_1.html', 'Basic Page'),
+    ('frontpage.html', 'Frontpage'),
+)
+
--- cntrct/urls.py
+++ cntrct/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cntrct.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns  += staticfiles_urlpatterns()
