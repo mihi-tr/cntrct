a73457a15d869820a08efe1f4f2db56c48e1a1fb
Michael Bauer <mihi@lo-res.org>
2014-07-13 13:38:06 +0200

    better error handling

--- campaigns/views.py
+++ campaigns/views.py
@@ -5,6 +5,8 @@ from django.core.context_processors import csrf
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponseRedirect
+from django.forms.util import ErrorList
+from django.db import IntegrityError
 import changeorg
 
 from campaigns.models import Campaign
@@ -33,16 +35,32 @@ def campaigns_widget(request):
 @xframe_options_exempt
 def add_widget(request):
     if request.method == 'POST':
-        form = CampaignForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render_to_response("campaigns/thanks.html",{},
+        urlform = AddFromURLForm(request.POST)
+        if urlform.is_valid():
+            url = urlform.cleaned_data['url']
+            cid = corg.get_petition_id(url)
+            petition = corg.get_petition(cid)
+            print petition
+            c = Campaign()
+            c.change_id = petition['petition_id']
+            c.title = petition['title']
+            c.url = petition['url']
+            c.overview=petition['overview']
+            c.signature_count=petition['signature_count']
+            c.image_url=petition['image_url']
+            try:
+                c.save()
+                return render_to_response("campaigns/thanks.html",{},
                     context_instance=RequestContext(request))
+            except IntegrityError:
+                errors =urlform._errors.setdefault("url", ErrorList())
+                errors.append(u"Someone already submitted this campaign")
+                            
+
+                
     else:
-        form = CampaignForm()
-    urlform = AddFromURLForm()
+        urlform = AddFromURLForm()
     c={
-        "form": form,
         "urlform": urlform }
     c.update(csrf(request))
     return render_to_response("campaigns/add_widget.html",c,
